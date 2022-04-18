1eb7c5d9bb57488980d71c8b9cfe303b300ff5b2
lianausane <102228831+lianausane@users.noreply.github.com>
2022-04-18 17:51:17 +0300

    bsNS

--- viss.py
+++ viss.py
@@ -4,20 +4,30 @@ a = "Uzdevums- Izveidot sarakstu kurā ir noteikts skaits skailū un šo skaitu 
 print(a)
 print( )
 sk = []
-n = int(input("Cik skaitļu būs saraksta? (20-50 skaitļi) "))
+while n < 20 or n > 50:
+    n = int(input("Cik skaitļu būs saraksta? (Ievadiet skaitli intervālā no 20 līdz 50.)"))
+    if n < 20:
+        print("šis skaitlis neder,jo ir mazākas par 20. Ievadi vēlreiz:")
+    elif n > 50:
+        print("šis skaitlis neder, jo ir lielāks par 50. Ievadi vēlreiz:")
 import random
 
 for i in range(n):
     sk.append(random.randrange(-100, 100))
 print(sk)
 
+
 x=int(input("Ievadiet skaitli! kuru pievienosim izveidotajam sarakstas intervālā no -100 līdz 100:"))
 sk.append(x)   
 
-sk.sort()
 print(sk)
-"""
+
+for num in sk:
+    if num % 2 == 0:
+        print(num)
+
+
 for num in sk:
     if num % 2 == 0:
         print(num)    
-"""        
+       
